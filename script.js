47bb223e3fdb2f3afb97ee4acd40c86ffbae8c40
HmccoyProgramming <88503108+HmccoyProgramming@users.noreply.github.com>
2021-12-08 18:43:13 -0800

    Add files via upload
    In this project I learned how to use CSS and JavaScript to create a rotating navigation on this page.

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click',() => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PRACTICE CODE // **************** 
+
+// let image1 = document.querySelector('.img1')
+
+// let yes = function() {
+//     image1.style.transition = '1s';
+//     // image1.style.transform = 'rotate(360deg)';
+//     if (image1.style.transform === '') {
+//         (image1.style.transform = 'rotate(-360deg)', image1.style.transform = 'scale(25%)')
+        
+//     } else if (image1.style.transform === 'rotate(-360deg)') {
+//         image1.style.transform = ''
+        
+//     }
+    
+// }
+
+
+// image1.addEventListener('click', function() {
+//     yes()
+// })
+
+
+/// ******************
+
+
